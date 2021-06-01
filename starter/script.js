04cfed0b4bec780348f2d8c7daa5f044e077e6fd
hanipnk <hanipnk@gmail.com>
2021-06-01 12:23:11 -0400

    Array Methods

--- starter/script.js
+++ starter/script.js
@@ -61,17 +61,38 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  // .textContent = 0
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+     <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}{</div>
+     <div class="movements__value">${mov}</div>
+  </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+//console.log(containerMovements.innerHTML);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -115,3 +136,172 @@ console.log([...arr, ...arr2]); // same result as 'arr.concat(arr2)'
 console.log(letters.join('-')); // ->a-b-c-d-e-f-g-h-i-j
 
 */
+
+/*
+
+// Looping arrays with 'forEach' method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement} `);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`); // 'Math.abs(value)' getting the absolute value without ('-')
+  }
+}
+console.log('---------forEach ----------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+// unlike 'for of loop' the FIRST parameter will be the elements of the array and SECOND parameter will be the index of the array.
+// THIRD will be the entire array itself
+// the ORDER MATTERS! and I CANNOT change the orders.
+// I CANNOT use 'break' and 'continue' in 'forEach' loop. if I need to use 'break' or'continue' I should use 'for of loop'
+
+
+*/
+
+/*
+
+// forEach with Maps and Sets
+
+// MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// 'forEach' in maps and sets, the order of parameters is also matter like 'forEach' in arrays.
+// 1) value 2) key 3) entiremap itself
+
+//SET
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'EUR', 'USD']);
+
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+  // ---> USD: USD
+  // ---> GBP: GBP
+  // ---> EUR: EUR
+});
+// I need to pass in an iterable for set. It also only takes the unique values without duplicates
+// There is NO 'key' and 'index' for set
+
+*/
+
+/*
+
+// Coding Challenge # 1
+
+// // MY OWN SOLUTION
+// const juliaData1 = [3, 5, 2, 12, 7];
+// const juliaData2 = [9, 16, 6, 8, 3];
+// const kateData1 = [4, 1, 15, 8, 3];
+// const kateData2 = [10, 5, 6, 1, 4];
+
+// const juliaData1Correct = juliaData1.slice(1, -2);
+// console.log(juliaData1Correct); // ---> (2) [5, 2]
+// //console.log(juliaData1);
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   dogsJulia.forEach(function (age, i) {
+//     if (age >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${age}years old`);
+//     } else if (age < 3 && age !== 1) {
+//       console.log(
+//         `Dog number ${i + 1} is still a puppy, and is ${age}years old`
+//       );
+//     } else if (age === 1) {
+//       console.log(
+//         `Dog number ${i + 1} is still a puppy, and is ${age}year old`
+//       );
+//     }
+//   });
+//   dogsKate.forEach(function (age, i) {
+//     if (age >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${age}years old`);
+//     } else if (age < 3 && age !== 1) {
+//       console.log(
+//         `Dog number ${i + 1} is still a puppy, and is ${age}years old`
+//       );
+//     } else if (age === 1) {
+//       console.log(
+//         `Dog number ${i + 1} is still a puppy, and is ${age}year old`
+//       );
+//     }
+//   });
+// };
+
+// checkDogs(juliaData1Correct, kateData1);
+
+
+// Solution
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  console.log(dogsJuliaCorrected);
+  const allDogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(allDogs);
+  allDogs.forEach(function (age, i) {
+    if (age >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age}years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+*/
+
+// Map Method in Arrays (Map Method does NOT mutate the original array and returns a NEW array with new elements)
+
+const eurToUsd = 1.1;
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+console.log(movements);
+console.log(movementsUSD);
+
+// Using Map Method in Arrow function
+const movementsUSDArrow = movements.map(mov => mov * eurToUsd);
+console.log(movementsUSDArrow);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+
+  // if (mov > 0) {
+  //   return `Movement ${i + 1}: You deposited ${mov}`;
+  // } else {
+  //   return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+  // }
+);
+console.log(movementsDescriptions);
+
+// Map method needs to be stored in a variable since it returns a NEW array.
+// Unlike 'forEach' which prints the elements one by one it returns a whole new array.
