2b365820598cbbb1497d78511b08a29b7f653dc5
hanipnk <hanipnk@gmail.com>
2021-06-02 12:30:53 -0400

    Map,Filter,Reduce

--- starter/script.js
+++ starter/script.js
@@ -61,6 +61,7 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
   // .textContent = 0
@@ -72,7 +73,7 @@ const displayMovements = function (movements) {
      <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}{</div>
-     <div class="movements__value">${mov}</div>
+     <div class="movements__value">${mov}€</div>
   </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -81,6 +82,71 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 //console.log(containerMovements.innerHTML);
+
+// Display Balance to the app with using 'reduce' method
+const calcDisplayBalance = function (movments) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} €`;
+};
+calcDisplayBalance(account1.movements);
+
+//Display Summary to the app
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calcDisplaySummary(account1.movements);
+
+// Computing Usernames
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+// const user = 'Steven Thomas Williams'; // stw
+
+createUsernames(accounts);
+
+//console.log(accounts);
+
+// const createusername = function (accs) {
+//   accs.forEach(function (acc) {
+//     acc.username = acc.owner
+//       .toLowerCase()
+//       .split(' ')
+//       .map(function (name) {
+//         return name[0];
+//       })
+//       .join('');
+//   });
+// };
+// createusername(accounts);
+// console.log(accounts);
+
+// I must use 'map' in '104' because if i use 'forEach' it returns a string not a new array.
+// So I CANNOT use 'join' in that case. 'join' is a method --> 'array' to 'string'
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -91,8 +157,6 @@ displayMovements(account1.movements);
 //   ['EUR', 'Euro'],
 //   ['GBP', 'Pound sterling'],
 // ]);
-
-//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -270,6 +334,8 @@ checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
 */
 
+/*
+
 // Map Method in Arrays (Map Method does NOT mutate the original array and returns a NEW array with new elements)
 
 const eurToUsd = 1.1;
@@ -305,3 +371,129 @@ console.log(movementsDescriptions);
 
 // Map method needs to be stored in a variable since it returns a NEW array.
 // Unlike 'forEach' which prints the elements one by one it returns a whole new array.
+
+*/
+
+/*
+// Filter Method
+
+//Filter method creates a 'NEW Array' for the elements that passed certain condition so I need a variable for the new array like 'Map'
+// 'RETURN' is needed same as 'MAP'
+// I can also have index and the entire array as parameters like 'forEach' and 'Map' but it's not often used at all.
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+// Using 'For of loop'
+const depositFor = [];
+for (const mov of movements) {
+  if (mov > 0) {
+    depositFor.push(mov);
+  }
+}
+console.log(depositFor);
+
+// Using Filter Method with arrow function
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);
+
+*/
+
+/*
+
+// Reduce Method (Reduce always gets 'accumulator' as a first parameter and needs a initial value as a second parameter)
+
+// Accumulator --> SNOW BALL (keeps adding the value that I ultimately return)
+// Reduce Method does NOT creates an array but one ultimate value instead
+
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 100);
+// as a second parameter I set '0' for the initial value.
+// '0' will be the initial value of the accumulator in the first loop iteration
+console.log(balance);
+
+// Using with Arrow Function
+const balanceArrow = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balanceArrow);
+
+// Using 'for of loop'
+let balance2 = 0;
+for (const mov of movements) {
+  balance2 += mov;
+}
+console.log(balance2);
+
+// Displaying Maximum Value with using 'Reduce Method'
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+
+console.log(max);
+// if 'acc' = 'mov', it simply returns mov so I dont have to specify the case of equal
+// I should not set '0' as a initial value because if I want to get a MINIMUM value, it will not work.
+
+*/
+
+/*
+// Coding Challenge #2
+
+
+//Solution
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+  //console.log(humanAges);
+  const adultDogs = humanAges.filter(age => age >= 18);
+  //console.log(adultDogs);
+  const averagedogs =
+    adultDogs.reduce((acc, age) => acc + age, 0) / adultDogs.length;
+  return averagedogs;
+
+  //another way of getting average (good use case)
+  // const average = adultDogs.reduce((acc,age,i,arr)=>acc+age/arr.length,0)
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+*/
+
+/*
+
+// Chaining Methods (I can only chain a method if the first one returns an array)
+//I CANNOT chain a method after 'reduce' because it only returns a single value
+const eurToUsd = 1.1;
+
+const totalDepositinUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositinUSD);
+
+// Solution of how to DEBUG
+// Since I chain all the methods to get a result, it is hard to find if there is a mistake
+// I should check out the current array in the NEXT array method which has chained on that filter.
+// 'return' is needed after 'console.log' because it is now more than one line of code.
+
+const totalDepositinUSD2 = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    //console.log(arr); // (5) [200, 450, 3000, 70, 1300] X 5
+    return mov * eurToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositinUSD2);
+
+*/
